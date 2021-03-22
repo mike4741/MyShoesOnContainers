3b9e871c51e5bdaa97fcc8dc8ae0d7baa4648dc9
mike4741 <64281489+mike4741@users.noreply.github.com>
2021-03-22 16:33:59 -0500

    Add project files.

---
+++ ProductCatalogAPI/Data/CatalogContext.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalogAPI.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogAPI.Data
+{
+    public class CatalogContext : DbContext
+    {
+
+        public CatalogContext(DbContextOptions options) : base (options)
+        {
+ 
+        }
+       
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+        public DbSet<CatalogSize> CatalogSizes { get; set; }
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CatalogBrand>(e =>
+            {
+                e.Property(b => b.Id)
+                .ValueGeneratedOnAdd();
+                e.Property(b => b.Brand)
+                .HasMaxLength(100)
+                .IsRequired();
+            });
+            modelBuilder.Entity<CatalogType>(t =>
+            {
+                t.Property(t => t.Id)
+                .ValueGeneratedOnAdd();
+                t.Property(t => t.Type)
+                .HasMaxLength(100)
+                .IsRequired();
+            });
+             modelBuilder.Entity<CatalogSize>(s => 
+             {
+                 s.Property(s => s.Id)
+                 .ValueGeneratedOnAdd();
+                 s.Property(s => s.Size)
+                 .IsRequired();
+            });
+            modelBuilder.Entity<CatalogItem>(i =>
+            {
+                i.Property(i => i.Id)
+                .ValueGeneratedOnAdd();
+                i.Property(i => i.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+                i.Property(i => i.PictureUrl)
+                .IsRequired();
+                i.Property(i => i.Price)
+                .IsRequired();
+                i.Property(i => i.Description);
+                i.HasOne(c => c.CatalogType)
+                .WithMany()
+                .HasForeignKey(c => c.CatalogTypeId);
+                i.HasOne(s => s.CatalogSize)
+                .WithMany()
+                .HasForeignKey(s => s.CatalogSizeId);
+                i.HasOne(b => b.CatalogBrand)
+                .WithMany()
+                .HasForeignKey(b => b.CatalogBrandId);
+            });
+        }
+
+    }
+}
---
+++ ProductCatalogAPI/Data/CatalogSeed.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalogAPI.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogAPI.Data
+{
+    public static  class CatalogSeed
+    {
+        public static void  Seed(CatalogContext catalogContext)
+        {
+            catalogContext.Database.Migrate();
+            if (!catalogContext.CatalogBrands.Any())
+            {
+                catalogContext.CatalogBrands.AddRange(GetCatalogBrands());
+                catalogContext.SaveChanges();
+            }
+           if  (!catalogContext.CatalogSizes.Any())
+            {
+                catalogContext.CatalogSizes.AddRange(GetCatalogSize());
+                catalogContext.SaveChanges();
+
+            }
+           if(!catalogContext.CatalogTypes.Any())
+            {
+                catalogContext.CatalogTypes.AddRange(GetCatalogType());
+                catalogContext.SaveChanges();
+
+            }
+           if (!catalogContext.CatalogItems.Any())
+            {
+                catalogContext.CatalogItems.AddRange(GetCatalogItem());
+                catalogContext.SaveChanges();
+            }
+
+        }
+
+        private static IEnumerable< CatalogItem> GetCatalogItem()
+        {
+            return new List<CatalogItem>() {
+                 new CatalogItem
+                 {
+                             Name = "Adidds",
+                      Description = "BasketBall Shoes", 
+                            Price = 150,
+                       PictureUrl = "http://externalcatalogbaseurltoberplaced/api/pic/1",
+                      
+                    CatalogBrandId =1,
+                    CatalogTypeId =1,
+                    CatalogSizeId = 3     
+                 },
+                      new CatalogItem
+                 {
+                             Name = "Adidds",
+                      Description = "walking Shoes",
+                            Price = 180,
+                       PictureUrl = "http://externalcatalogbaseurltoberplaced/api/pic/2",
+
+                    CatalogBrandId =1,
+                    CatalogTypeId =3,
+                    CatalogSizeId = 2
+                 },
+                           new CatalogItem
+                 {
+                             Name = "NIke",
+                      Description = "walking  Shoes",
+                            Price = 170,
+                       PictureUrl = "http://externalcatalogbaseurltoberplaced/api/pic/6",
+
+                    CatalogBrandId =2,
+                    CatalogTypeId =2,
+                    CatalogSizeId = 4
+                 },
+             };
+        }
+
+        private static IEnumerable<CatalogType> GetCatalogType()
+        {
+            return new List<CatalogType>()
+            {
+                new CatalogType
+                {
+                     Type = "BasketBall"
+                },
+                new CatalogType
+                {
+                    Type = "Runing"
+                },
+                new CatalogType
+                {
+                    Type = "Walking"
+                }
+                 
+
+            };
+        }
+
+        private static IEnumerable< CatalogSize> GetCatalogSize()
+        {
+            return new List<CatalogSize>()
+            {
+                 new CatalogSize
+                 {
+                     Size =8
+                 },
+                 new CatalogSize
+                 {
+                     Size =9
+                 },
+                 new CatalogSize
+                 {
+                    Size = 10
+                 },
+                 new CatalogSize
+                 {
+                     Size =12
+                 },
+                 new CatalogSize
+                 {
+                     Size =13
+                 }
+                 
+            };
+        }
+
+        private static IEnumerable<CatalogBrand> GetCatalogBrands()
+        {
+            return new List<CatalogBrand>
+            {
+                new CatalogBrand
+                {
+                    Brand = "Adidds"
+                },
+                new CatalogBrand
+                {
+                    Brand = "Nike"
+                },
+                new CatalogBrand
+                {
+                    Brand = "puma"
+                }
+            };
+        }
+    }
+}
